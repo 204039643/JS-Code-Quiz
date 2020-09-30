21c83db8365547331a64e075c9e3763b79b36d51
justin sykes <justin.sykes@ge.com>
2020-09-30 13:30:10 -0400

    Completed quiz section, added highscores.html

--- script.js
+++ script.js
@@ -1,74 +1,142 @@
 //DOM variables
-var startButton = document.querySelector("#start-button")
-var containerEl = document.querySelector("#container")
-var clearEl = document.querySelector("#clear")
-
+var startButton = document.getElementById("start-button");
+var startSectionEl = document.getElementById("startSection");
+var questionsEl = document.getElementById("questions");
+var titleEl = document.getElementById("title");
+var choicesEl = document.getElementById("choices");
+var endScreenEl = document.getElementById("endScreen");
+var submitEl = document.getElementById("submit");
+var timerEl = document.getElementById("time");
+var feedbackEl = document.getElementById("feedback");
+var answerEl = document.getElementById("answerDiv");
+var time = 75;
+var timerId;
 
 //JS variables
-var Q1Answers = ["1. To parameterize a function call", "2. To complete a thought", "3. To define an object", "4. To settle a score!"];
-var Q2Answers = ["1. To parameterize a function call", "2. To complete a thought", "3. To define an object", "4. To settle a score!"];
-var Q3Answers = ["1. To parameterize a function call", "2. To complete a thought", "3. To define an object", "4. To settle a score!"];
-var Q4Answers = ["1. To parameterize a function call", "2. To complete a thought", "3. To define an object", "4. To settle a score!"];
-var answerKey = {
-    questionOne: "A1",
-    questionTwo: "A3",
-    questionThree: "A2",
-    questionFour: "A4",
+
+var questionId = 0;
+
+//array of objects
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title:
+            "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
+
+//function definitions
+
+function startTimer() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+    }
 }
-//function defintions
+
+function endQuiz() {
+    clearInterval(timerId);
+    timerEl.textContent = time;
+}
+
+
+
 function startQuiz() {
     console.log("Quiz has started!");
-    console.log(window);
-    clearcontent('clear');
-    newBlock();
+    startSectionEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    timerId = setInterval(startTimer, 1000);
+    timerEl.textContent = time;
+    newQuestion();
 }
-function clearcontent(elementID) {
-    document.getElementById(elementID).innerHTML = "";
-}
-//Create a new question block
-function newBlock() {
-    var newBlock = document.createElement("h1")
-    var newQuestion = document.createTextNode("Where are arguments used?")
-    var questionId = 1
-    newBlock.appendChild(newQuestion);
-    currentBlock = document.getElementById("q-block");
-    document.body.insertBefore(newBlock, currentBlock);
-    newBlock.style.paddingLeft = "20%";
-    newBlock.style.paddingTop = "5%";
-    newBlock.style.paddingBottom = "4%";
-    console.log("This is question " + questionId);
+
+//Create a new question with 4 multiple choice answers
+function newQuestion() {
+
+    var currentQuestion = questions[questionId];
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+
 
     //Create 4 answer buttons for the current question    
-    for (i = 0; i < Q1Answers.length; i++) {
-        var answer = Q1Answers[i];
-        var newButton = document.createElement("h4");
+    currentQuestion.choices.forEach((choice, i) => {
+
         var button = document.createElement("button");
-        button.textContent = answer;
-        newButton.appendChild(button);
-        currentAnswer = document.getElementById("a-block");
-        document.body.insertBefore(newButton, currentAnswer);
-        newButton.style.paddingLeft = "20%";
-        newButton.children[0].style.backgroundColor = "purple";
-        newButton.children[0].style.color = "white";
-        newButton.children[0].id = "aButton" + [i];
-        console.log(answer);
-        console.log(button);
-    }
-    aButton0.addEventListener("click", aButtonClick);
-    aButton1.addEventListener("click", aButtonClick);
-    aButton2.addEventListener("click", aButtonClick);
-    aButton3.addEventListener("click", aButtonClick);
+        button.setAttribute("class", "choice");
+        button.setAttribute("value", choice);
+        button.textContent = i + 1 + ". " + choice;
+        button.onclick = aButtonClick;
+        choicesEl.appendChild(button);
+
+    })
+
+
+
 }
 
 function aButtonClick() {
-    console.log("You clicked " + this.id);
-    //check answer
-    if (questionId = 1 && this.id === "aButton0") {
-        alert("Correct!");
+    // check if user guessed wrong
+    if (this.value !== questions[questionId].answer) {
+        // penalize time
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        // display new time on page
+        timerEl.textContent = time;
+
+        feedbackEl.textContent = "Wrong!";
     } else {
-        alert("Incorrect!");
+        feedbackEl.textContent = "Correct!";
+    }
+
+    // flash right/wrong feedback on page
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // move to next question
+    questionId++;
+
+    // check if we've run out of questions
+    if (questionId === questions.length) {
+        endQuiz();
+    } else {
+        newQuestion();
     }
 
 }
+
 //events
 startButton.addEventListener("click", startQuiz);
